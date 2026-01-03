096dd6e3a5a46e51e2b5a15d9a844e0179a191e4
musaSaadatyar <musa.saadatyar@email.com>
2026-01-03 17:14:48 +0330

    params in exprerss js

--- server.js
+++ server.js
@@ -1,18 +1,30 @@
 const express = require("express");
 const app = express();
 
+const friends = [
+  { id: 0, name: "John" },
+  { id: 1, name: "Jane" },
+  { id: 2, name: "Doe" },
+  { id: 3, name: "Smith" },
+  { id: 4, name: "Emily" },
+  { id: 5, name: "Michael" },
+];
+
 app.get("/", (req, res) => {
   res.send("Hello from Express.js");
 });
 
 app.get("/friends", (req, res) => {
-  res.send(`
-        <ul
-        <li>John</li>
-        <li>Jane</li>
-        <li>Doe</li>
-        </ul>
-    `);
+  res.send(friends);
+});
+
+app.get("/friends/:id", (req, res) => {
+  const param = req.params.id;
+  const frendId = friends.find((item) => item.id == param);
+  if (!frendId) {
+    return res.status(404).json({ error: "Friend not found" });
+  }
+  res.send(frendId);
 });
 
 const PORT = 3100;
