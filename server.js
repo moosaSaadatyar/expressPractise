aea02301f57795f06c19d65100e9a7f6cd6a29f5
musaSaadatyar <musa.saadatyar@email.com>
2026-02-20 17:59:36 +0330

    server by disine patern

--- server.js
+++ server.js
@@ -1,72 +1,33 @@
+const friendsController = require("./controllers/friends.controler");
 const express = require("express");
 const app = express();
 
-const friends = [
-  { id: 0, name: "John" },
-  { id: 1, name: "Jane" },
-  { id: 2, name: "Doe" },
-  { id: 3, name: "Smith" },
-  { id: 4, name: "Emily" },
-  { id: 5, name: "Michael" },
-];
+
 
 app.get("/", (req, res) => {
   res.send("Hello from Express.js");
 });
 
-app.get("/friends", (req, res) => {
-  res.send(friends);
-});
+//getfriends controller
+app.get("/friends",friendsController.getFriends);
 
 app.use(express.json());
 
-app.get("/friends/:id", (req, res) => {
-  const param = req.params.id;
-  const frendId = friends.find((item) => item.id == param);
-  if (!frendId) {
-    const statusCode = res.status(404).json({ error: "Friend not found" });
-    return statusCode;
-  }
-  res.send(frendId);
-});
+app.get("/friends/:id",friendsController.getFriendById );
 
-app.post("/friends", (req, res) => {
-  let newFriends = {
-    id: friends.length,
-    name: req.body.name,
-  };
+// postfriends controller
+app.post("/friends",friendsController.postFriends );
 
-  if (!req.body.name) {
-    return res.status(400).json({
-      error: "friend not found",
-    });
-  }
+//putfriends controller
+app.put("/friends/:id",friendsController.putFriends);
 
-  friends.push(newFriends);
-  res.json(newFriends);
-});
+//deletefriends controller
+app.delete("/friends/:id",friendsController.deleteFriends);
 
-app.put("/friends/:id", (req, res) => {
-  const friendId = parseInt(req.params.id);
-  const friend = friends.find((item) => item.id === friendId);
-  if (!friendId || !req.body.name) {
-    return res.status(404).json({
-      error: "there was not friends with this Info",
-    });
-  }
-  friend.name = req.body.name;
-  res.send(friend);
-});
 
-app.delete("/friends/:id", (req, res) => {
-  const friendId = parseInt(req.params.id);
-  const friendIndex = friends.findIndex((item) => item.id === friendId);
-  if (friendIndex === -1) {
-    return res.status(404).json({ error: "Friend not found" });
-  }
-  const deletedFriend = friends.splice(friendIndex, 1);
-  res.json(deletedFriend[0]);
-});
+/**
+ * run server
+ */
 const PORT = 3100;
 app.listen(PORT, (err, res) => {
   console.log(`server run in ${PORT}`);
