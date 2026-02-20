b252973b6a4053190f9386d76ab731a1060d0d2e
musaSaadatyar <musa.saadatyar@email.com>
2026-02-20 17:59:12 +0330

    friends model

---
+++ models/friends.models.js
@@ -0,0 +1,10 @@
+const friends = [
+  { id: 0, name: "John" },
+  { id: 1, name: "Jane" },
+  { id: 2, name: "Doe" },
+  { id: 3, name: "Smith" },
+  { id: 4, name: "Emily" },
+  { id: 5, name: "Michael" },
+];
+
+module.exports = { friends };
