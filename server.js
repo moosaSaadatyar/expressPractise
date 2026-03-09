821d5c916763a9a2081f231df3dcdec830c39bfb
musaSaadatyar <musa.saadatyar@email.com>
2026-03-09 22:16:30 +0330

    add layout

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
+const express = require("express");
+const path = require("path");
+
 const friendsRouter = require("./routers/friends.router");
 const messagesRouter = require("./routers/masseges.router");
-const express = require("express");
+
 const app = express();
 const PORT = 3100;
 
 app.use(express.json());
+app.set("view engine", "hbs");
+app.set("views", "views");
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "This Is a Title...!!!",
+    caption: "this Is a cation about  you",
+  });
+});
+
+app.use("/site", express.static(path.join(__dirname, "public")));
 app.use("/messages", messagesRouter);
 app.use("/friends", friendsRouter);
 
